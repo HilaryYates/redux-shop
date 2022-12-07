8b92664513ce9b722fc64d42e03b676622e6a4f3
hilaryyates <hilryyates@gmail.com>
2022-12-07 07:37:51 -0500

    provided store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,3 +2,5 @@ import { combineReducers } from "redux";
 import { productReducer } from "./productReducer";
 
 const reducers = combineReducers({ productReducer });
+
+export default reducers;
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -4,8 +4,8 @@ export const initialState = {
   products: [{ id: 1, title: "redux-project", category: "redux" }],
 };
 
-export const productReducer = (state, action) => {
-  switch ((action.type, { type, payload })) {
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case ActionTypes.SET_PRODUCTS:
       return state;
 
