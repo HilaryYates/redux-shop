7de271b4b60fd61c79d3412754a7b334ec5a7c86
hilaryyates <hilryyates@gmail.com>
2022-12-07 08:04:07 -0500

    components in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import Header from "./redux/containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductComponent from "./redux/containers/ProductComponent";
+import ProductDetail from "./redux/containers/ProductDetail";
+import ProductListing from "./redux/containers/ProductListing";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path='/' exact component={<ProductListing />} />
+          <Route
+            path='/product/:productId'
+            exact
+            component={<ProductDetail />}
+          ></Route>
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
